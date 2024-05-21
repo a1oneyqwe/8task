ed1a5ba1d213686b66c0b6971026379a490a38b6
s29076 <s29076@pjwstk.edu.pl>
2024-05-21 14:56:34 +0200

    qwe

---
+++ WebApplication1/WebApplication1/Controllers/TripsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly ITripRepository _tripRepository;
+
+        public TripsController(ITripRepository tripRepository)
+        {
+            _tripRepository = tripRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTrips([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            var trips = await _tripRepository.GetTrips(page, pageSize);
+            var totalTrips = await _tripRepository.GetTripsCount();
+            var totalPages = (int)Math.Ceiling((double)totalTrips / pageSize);
+
+            return Ok(new
+            {
+                pageNum = page,
+                pageSize,
+                allPages = totalPages,
+                trips = trips.Select(t => new
+                {
+                    t.Name,
+                    t.Description,
+                    t.DateFrom,
+                    t.DateTo,
+                    t.MaxPeople,
+                    Countries = t.CountryTrips.Select(ct => new { ct.Country.Name }),
+                    Clients = t.ClientTrips.Select(ct => new { ct.Client.FirstName, ct.Client.LastName })
+                })
+            });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Country
+    {
+        [Key]
+        public int IdCountry { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<CountryTrip> CountryTrips { get; set; }
+    }
+}
